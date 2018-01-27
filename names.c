75b91cbdd09558969ad017cd1661ca3c2292ca3b
jlemke <jrlemke7@huskers.unl.edu>
2018-01-26 19:58:01 -0600

    Added my name to names file

--- names.c
+++ names.c
@@ -1,4 +1,4 @@
-/* Author: Andrew Thurman
+/* Author: Andrew Thurman and Joshua Lemke
 *Due Date: 1/26/2018
 *Class: CSCE155E
 *Assignment: 1
@@ -9,7 +9,7 @@
 
 int main(int argc,char **argv) {
 
-printf("Andrew Thurman \n");
+printf("Andrew Thurman and Joshua Lemke \n");
 
 return 0;
 }
