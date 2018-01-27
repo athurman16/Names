8ffa67e1623fa1de61f3edd59c0b5799b165a178
athurman16 <athurman16@huskers.unl.edu>
2018-01-26 19:53:04 -0600

    Created Hello World program

---
+++ names.c
@@ -0,0 +1,15 @@
+/* Author: Andrew Thurman
+*Due Date: 1/26/2018
+*Class: CSCE155E
+*Assignment: 1
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc,char **argv) {
+
+printf("Andrew Thurman \n");
+
+return 0;
+}
